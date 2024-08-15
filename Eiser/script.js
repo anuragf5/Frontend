18b2daca1c771a4b867c7ca05d5f599c2878bc46
Anurag <anuragf5@gmail.com>
2024-08-15 18:43:35 +0530

    Add Java Script file for Eiser website

---
+++ Eiser/script.js
@@ -0,0 +1,31 @@
+document.getElementById("home_page").onclick = function () {
+  window.open("eiser.html", "_self");
+};
+
+document.getElementById("shopping_cart").onclick = function () {
+  window.open("gift-card-page.html", "_self");
+};
+
+document.getElementById("gift_card").onclick = function () {
+  window.open("gift-card-page.html", "_self");
+};
+
+document.getElementById("track_order").onclick = function () {
+  window.open("track-order-page.html", "_self");
+};
+
+document.getElementById("contact_us").onclick = function () {
+  window.open("contact-us-page.html", "_self");
+};
+
+document.getElementById("shop_category").onclick = function () {
+  window.open("shopping-category-page.html", "_self");
+};
+
+document.getElementById("product_checkout").onclick = function () {
+  window.open("product_checkout.html", "_self");
+};
+
+document.getElementById("product_details").onclick = function () {
+  window.open("product_detail_page.html", "_self");
+};
